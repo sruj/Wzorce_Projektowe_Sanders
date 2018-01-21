bc659a5d889c7fd0b1530709d830bb5534f57101
sruj <chryplewiczpawel@wp.pl>
2018-01-21 19:52:59 +0100

    comment

--- [06] Dekorator (str135)/Decorator.php
+++ [06] Dekorator (str135)/Decorator.php
@@ -1,5 +1,10 @@
 <?php
 
 abstract class Decorator extends IComponent{
-    
+    //Dziedziczy metody getSite() i getPrice()
+    //Jest to klasa abstrakcyjna i
+    //nie ma konieczności implementacji żadnej z metod
+    //Chodzi o to, aby zachować referencję do klasy Component
+    //public function getSite() { }
+    //public function getPrice() { }
 }
